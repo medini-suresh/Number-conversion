8cc19819b38024c8e11efc836c2967b411df6389
Medini Suresh <76805579+medini-suresh@users.noreply.github.com>
2022-11-11 21:08:06 +0530

    Add files via upload

---
+++ dectoBin.py
@@ -0,0 +1,24 @@
+def decToBinary(n):
+     
+    # array to store
+    # binary number
+    binaryNum = [0] * n;
+ 
+    # counter for binary array
+    i = 0;
+    while (n > 0):
+ 
+        # storing remainder
+        # in binary array
+        binaryNum[i] = n % 2;
+        n = int(n / 2);
+        i += 1;
+ 
+    # printing binary array
+    # in reverse order
+    for j in range(i - 1, -1, -1):
+        print(binaryNum[j], end = "");
+ 
+# Driver Code
+n = 17;
+decToBinary(n);
